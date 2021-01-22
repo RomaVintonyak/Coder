701209a0bdd9b33a60a3f9a5d742746235033118
RomaVintonyak <19roman23@gmail.com>
2021-01-22 16:48:11 +0200

    finished about us & add jquery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 jQuery(document).ready(function(){
   "use script";
-
+  /*smoth scroll to section*/
+  var navLinks = $(".nav__link");
+  navLinks.on("click", function(event){
+    event.preventDefault();
+    var linkId = $(this).attr("href");
+    var scrollPos = $(linkId).offset().top;
+    $("html, body").animate({
+      scrollTop: scrollPos
+    }, 800);
+  });
+  /*menuScroll add class*/
+  $("#menuScroll").singlePageNav({
+    offset: 0,
+	  currentClass: 'nav__link--active',
+    currentThreshold: 0,
+    duration: 1000,
+    effect: 'swing',
+  });
 });
