0d0a8af38d4a6de09c21d794739b43240362ac3d
RomaVintonyak <19roman23@gmail.com>
2021-01-26 12:10:22 +0200

    create animation to we do section

--- js/main.js
+++ js/main.js
@@ -18,4 +18,37 @@ jQuery(document).ready(function(){
     duration: 1000,
     effect: 'swing',
   });
+  /*hove efect to we do collum icon*/
+  var ideaIcon = $("#ideaIcon"),
+      conceptIcon = $("#conceptIcon"),
+      designIcon = $("#designIcon"),
+      developIcon = $("#developIcon");
+  ideaIcon.hover(function(){
+    conceptIcon.addClass("border__icon");
+  });
+  ideaIcon.mouseleave(function(){
+    conceptIcon.removeClass("border__icon");
+  });
+  conceptIcon.hover(function(){
+    designIcon.addClass("border__icon");
+  });
+  conceptIcon.mouseleave(function(){
+    designIcon.removeClass("border__icon");
+  });
+  designIcon.hover(function(){
+    developIcon.addClass("border__icon");
+  });
+  designIcon.mouseleave(function(){
+    developIcon.removeClass("border__icon");
+  });
+  developIcon.hover(function(){
+    ideaIcon.addClass("border__icon");
+    conceptIcon.addClass("border__icon");
+    designIcon.addClass("border__icon");
+  });
+  developIcon.mouseleave(function(){
+    ideaIcon.removeClass("border__icon");
+    conceptIcon.removeClass("border__icon");
+    designIcon.removeClass("border__icon");
+  });
 });
